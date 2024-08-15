49f31868cf8ee477be57c5e154633a3f9238c39a
gigiwang <gigiwangjob@gmail.com>
2024-08-14 21:22:46 -0500

    invoice

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,11 +1,15 @@
 import CustomersTable from "@/app/ui/customers/table";
 import { fetchCustomers } from "@/app/lib/data";
+import { lusitana } from "@/app/ui/fonts";
+import { Suspense } from "react";
 
 export default async function Page() {
   const customers = await fetchCustomers();
   return (
-    <div>
-      <CustomersTable customers={customers} />
+    <div className="w-full">
+      <Suspense>
+        <CustomersTable customers={customers} />
+      </Suspense>
     </div>
   );
 }

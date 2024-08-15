e0ae21392381186961cea7eb81380e9bff2a5c96
gigiwang <gigiwangjob@gmail.com>
2024-08-14 20:48:25 -0500

    invoice

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import CustomersTable from "@/app/ui/customers/table";
+import { fetchCustomers } from "@/app/lib/data";
+
+export default async function Page() {
+  const customers = await fetchCustomers();
+  return (
+    <div>
+      <CustomersTable customers={customers} />
+    </div>
+  );
 }

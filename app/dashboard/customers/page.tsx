1e615bd0396f15f9983c525c97a377a2b413ce70
gigiwang <gigiwangjob@gmail.com>
2024-08-14 21:05:24 -0500

    invoice

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,8 @@
 import CustomersTable from "@/app/ui/customers/table";
-import FormattedCustomersTable from "@/app/lib/definitions";
 import { fetchCustomers } from "@/app/lib/data";
 
 export default async function Page() {
-  const customers: FormattedCustomersTable[] = await fetchCustomers();
+  const customers = await fetchCustomers();
   return (
     <div>
       <CustomersTable customers={customers} />
